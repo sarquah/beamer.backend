0ccf6ccada32d301e5348815bc9af4db519bffe8
Stephen Sarquah <stephen.sarquah@gmail.com>
2019-06-13 13:00:39 +0200

    Added a project controller

---
+++ ProjectManagementAPI/Controllers/ProjectController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectManagementAPI.Models;
+
+namespace ProjectManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly ProjectContext _context;
+
+        public ProjectController(ProjectContext context)
+        {
+            _context = context;
+
+            if (_context.Projects.Count()==0)
+            {
+                _context.Projects.Add(new Project { Name = "Empty project" });
+                _context.SaveChanges();
+            }
+        }
+
+        // GET: api/Project
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Project>>> GetProjects()
+        {
+            return await _context.Projects.ToListAsync();
+        }
+
+        // GET: api/Project/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Project>> GetProject(long id)
+        {
+            var project = await _context.Projects.FindAsync(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+            return project;
+        }
+    }
+}
---
+++ ProjectManagementAPI/Models/ProjectContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagementAPI.Models
+{
+    public class ProjectContext : DbContext
+    {
+        public ProjectContext(DbContextOptions<ProjectContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Project> Projects { get; set; }
+    }
+}
