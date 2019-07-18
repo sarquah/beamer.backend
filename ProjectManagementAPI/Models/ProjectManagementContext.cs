d0013a7f94b893166811ada21ab33789d5fbe60b
Stephen Sarquah <stephen.sarquah@gmail.com>
2019-07-18 21:46:21 +0200

    Removed comments

--- ProjectManagementAPI/Models/ProjectManagementContext.cs
+++ ProjectManagementAPI/Models/ProjectManagementContext.cs
@@ -4,23 +4,9 @@ namespace ProjectManagementAPI.Models
 {
     public class ProjectManagementContext : DbContext
     {
-        public ProjectManagementContext(DbContextOptions<ProjectManagementContext> options) : base(options)
-        {
-        }
+        public ProjectManagementContext(DbContextOptions<ProjectManagementContext> options) : base(options){}
         public DbSet<Project> Projects { get; set; }
         public DbSet<Task> Tasks { get; set; }
         public DbSet<User> Users { get; set; }
-
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.Entity<Project>()
-        //        .HasOne(p => p.ProjectOwner)
-        //        .WithMany(u => u.Projects)
-        //        .OnDelete(DeleteBehavior.Restrict);
-        //    modelBuilder.Entity<Task>()
-        //        .HasOne(t => t.TaskOwner)
-        //        .WithMany(u => u.Tasks)
-        //        .OnDelete(DeleteBehavior.Restrict);
-        //}
     }
 }
