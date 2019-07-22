64db202a2552c3944b8b3f4ba177d790d8a6e8ba
Stephen Sarquah <stephen.sarquah@gmail.com>
2019-07-22 10:22:27 +0200

    Added lazy loading to models

--- ProjectManagementAPI/Domain/Models/Task.cs
+++ ProjectManagementAPI/Domain/Models/Task.cs
@@ -3,6 +3,6 @@
     public class Task : Activity
     {
         public long? ProjectId { get; set; }
-        public Project Project { get; set; }
+        public virtual Project Project { get; set; }
     }
 }
