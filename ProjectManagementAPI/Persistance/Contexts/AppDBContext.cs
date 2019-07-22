85623aa5db68d384ccae3850010d90cc0ccc2afb
Stephen Sarquah <stephen.sarquah@gmail.com>
2019-07-22 10:23:48 +0200

    Added lazy loading to dbcontext

--- ProjectManagementAPI/Persistance/Contexts/AppDBContext.cs
+++ ProjectManagementAPI/Persistance/Contexts/AppDBContext.cs
@@ -5,8 +5,8 @@ namespace ProjectManagementAPI.Models
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
-        public DbSet<Project> Projects { get; set; }
-        public DbSet<Task> Tasks { get; set; }
-        public DbSet<User> Users { get; set; }
+        public virtual DbSet<Project> Projects { get; set; }
+        public virtual DbSet<Task> Tasks { get; set; }
+        public virtual DbSet<User> Users { get; set; }
     }
 }
