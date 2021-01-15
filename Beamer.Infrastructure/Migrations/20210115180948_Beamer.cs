57da9e1a43fdbc205c791226b30e95a73cb56d33
Stephen Sarquah <stephen.sarquah@gmail.com>
2021-01-15 19:54:47 +0100

    Added migration files

---
+++ Beamer.Infrastructure/Migrations/20210115180948_Beamer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Beamer.Migrations
+{
+    public partial class Beamer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
