5fd5d32e329ac2fae4bceef0b27ab22605cd49cd
Stephen Sarquah <stephen.sarquah@gmail.com>
2021-02-06 14:17:33 +0100

    Added project-controller unit test

---
+++ Beamer.UnitTest/Controllers/ProjectControllerApi_UnitTest.cs
@@ -0,0 +1,121 @@
+﻿using AutoMapper;
+using Beamer.API.Controllers;
+using Beamer.Domain.Models;
+using Beamer.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using Task = System.Threading.Tasks.Task;
+
+namespace Beamer.UnitTest.Controllers
+{
+	public class ProjectControllerApi_UnitTest
+	{
+		private ProjectController _projectController;
+
+		public ProjectControllerApi_UnitTest()
+		{			
+			var project = new Project()
+			{
+				Id = 1,
+				Name = "Test Unit Project",
+				StartDate = new DateTime(2019, 8, 8),
+				EndDate = new DateTime(2019, 8, 12),
+				Description = "This is a unit test",
+				Status = EStatus.NotStarted,
+				TenantId = Guid.NewGuid()
+			};
+			var projects = new List<Project>()
+			{ 
+				project 
+			};
+			projects.Add(project);
+			var mockProjectService = new Mock<IProjectService>();
+			mockProjectService.Setup(service => service.GetProjects(It.IsAny<Guid>())).ReturnsAsync(projects);
+			mockProjectService.Setup(service => service.GetProject(It.IsAny<long>(), It.IsAny<Guid>())).ReturnsAsync(project);
+			mockProjectService.Setup(service => service.CreateProject(It.IsAny<Project>())).ReturnsAsync(true);
+			mockProjectService.Setup(service => service.UpdateProject(It.IsAny<long>(), It.IsAny<Project>())).ReturnsAsync(true);
+			mockProjectService.Setup(service => service.DeleteProject(It.IsAny<long>())).ReturnsAsync(true);
+			var mockMapper = new Mock<IMapper>();
+			_projectController = new ProjectController(mockProjectService.Object, mockMapper.Object);
+		}
+
+		[Fact]
+		public async Task GetProjects_Returns_ActionResultOfProjects()
+		{
+			// Arrange
+			var tenantId = Guid.NewGuid();
+			// Act
+			var response = await _projectController.GetProjects(tenantId);
+			// Assert
+			Assert.IsAssignableFrom<ActionResult<IEnumerable<ProjectDTO>>>(response);
+		}
+
+		[Fact]
+		public async Task GetProject_Returns_ActionResultOfProject()
+		{
+			// Arrange
+			var tenantId = Guid.NewGuid();
+			var projectId = 1;
+			// Act
+			var response = await _projectController.GetProject(projectId, tenantId);
+			// Assert
+			Assert.IsAssignableFrom<ActionResult<ProjectDetailsDTO>>(response);
+		}
+
+		[Fact]
+		public async Task CreateProject_Returns_CreatedAtActionResult()
+		{
+			// Arrange
+			var tenantId = Guid.NewGuid();
+			var project = new Project()
+			{
+				Id = 1,
+				Name = "Test Unit Project",
+				StartDate = new DateTime(2019, 8, 8),
+				EndDate = new DateTime(2019, 8, 12),
+				Description = "This is a unit test",
+				Status = EStatus.NotStarted,
+				TenantId = tenantId
+			};
+			// Act
+			var response = await _projectController.CreateProject(project);
+			// Assert
+			Assert.IsAssignableFrom<CreatedAtActionResult>(response);
+		}
+
+		[Fact]
+		public async Task UpdateProject_Returns_NoContentResult()
+		{
+			// Arrange
+			var tenantId = Guid.NewGuid();
+			var project = new Project()
+			{
+				Id = 1,
+				Name = "Test Unit Project Update",
+				StartDate = new DateTime(2019, 8, 8),
+				EndDate = new DateTime(2019, 8, 12),
+				Description = "This is a unit test update",
+				Status = EStatus.NotStarted,
+				TenantId = tenantId
+			};
+			// Act
+			var response = await _projectController.UpdateProject(project.Id, project);
+			// Assert
+			Assert.IsAssignableFrom<NoContentResult>(response);
+		}
+
+		[Fact]
+		public async Task DeleteProject_Returns_NoContentResult()
+		{
+			// Arrange
+			var projectId = 1;
+			// Act
+			var response = await _projectController.DeleteProject(projectId);
+			// Assert
+			Assert.IsAssignableFrom<NoContentResult>(response);
+		}
+	}
+}
